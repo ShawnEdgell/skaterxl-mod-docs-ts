2b5828cb405bf2f41c5ef1f22da23b25799abd03
shawn edgell <shawn_edgell@hotmail.com>
2024-02-22 07:44:37 -0500

    working template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,35 @@
 <script>
+	import { page } from '$app/stores';
 	import '../app.css';
+
+	// Reactive statement to get the current path
+	$: currentPath = $page.url.pathname;
 </script>
 
-<div class="min-h-screen">
-	<header class="bg-blue-300 flex justify-center p-4 sticky w-full top-0 z-10">
+<div class="min-h-screen bg-custom-black">
+	<header class="flex justify-center p-4 sticky w-full top-0 z-10">
 		<nav class="flex gap-8">
-			<a href="/">Home</a>
-			<a href="/gettingstarted">Getting Started</a>
-			<a href="/maps&gear">Maps & Gear</a>
-			<a href="/xlhub">XL Hub</a>
-			<a href="/stats&settings">Stats & Settings</a>
+			<a href="/" class={currentPath === '/' ? 'text-custom-blue' : 'text-white'}>Home</a>
+			<a
+				href="/gettingstarted"
+				class={currentPath === '/gettingstarted' ? 'text-custom-blue' : 'text-white'}
+				>Getting Started</a
+			>
+			<a href="/maps&gear" class={currentPath === '/maps&gear' ? 'text-custom-blue' : 'text-white'}
+				>Maps & Gear</a
+			>
+			<a href="/xlhub" class={currentPath === '/xlhub' ? 'text-custom-blue' : 'text-white'}
+				>XL Hub</a
+			>
+			<a
+				href="/stats&settings"
+				class={currentPath === '/stats&settings' ? 'text-custom-blue' : 'text-white'}
+				>Stats & Settings</a
+			>
 		</nav>
 	</header>
 
-	<div class="px-6 py-16 flex-grow flex flex-col items-center justify-center">
+	<div class="p-6 flex-grow flex flex-col items-center justify-center">
 		<slot />
 	</div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'custom-blue': '#4a9afb',
+        'custom-light-blue': '#e3e2e9', // Note: This color is the same as 'custom-light-gray'
+        'custom-black': '#101010',
+        'custom-gray': '#232323',
+        'custom-light-gray': '#e3e2e9', // Duplicate of 'custom-light-blue'
+        'custom-lime-green': '#d6f93d',
+        'custom-purple': '#a77ef2',
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
