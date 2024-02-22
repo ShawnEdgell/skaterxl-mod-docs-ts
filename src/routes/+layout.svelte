9128cdf2b2baf9319325159a7afe492ea3ad2fda
shawn edgell <shawn_edgell@hotmail.com>
2024-02-22 13:45:56 -0500

    kinda working, just need sticky sidebars

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,49 +6,51 @@
 	$: currentPath = $page.url.pathname;
 </script>
 
-<div class="flex flex-col min-h-screen bg-custom-black">
-	<header class="backdrop-blur-md text-sm flex justify-end sticky p-6 w-full top-0 z-10">
-		<nav class="flex gap-8">
-			<a href="/" class={currentPath === '/' ? 'text-custom-blue' : 'text-white'}>Home</a>
-			<a
-				href="/gettingstarted"
-				class={currentPath === '/gettingstarted' ? 'text-custom-blue' : 'text-white'}
-				>Getting Started</a
-			>
-			<a href="/maps&gear" class={currentPath === '/maps&gear' ? 'text-custom-blue' : 'text-white'}
-				>Maps & Gear</a
-			>
-			<a href="/xlhub" class={currentPath === '/xlhub' ? 'text-custom-blue' : 'text-white'}
-				>XL Hub</a
-			>
-			<a
-				href="/stats&settings"
-				class={currentPath === '/stats&settings' ? 'text-custom-blue' : 'text-white'}
-				>Stats & Settings</a
-			>
-		</nav>
-	</header>
+<div class="w-full flex justify-center">
+	<div class="max-w-custom-8xl">
+		<header class="backdrop-blur-md text-sm justify-end flex sticky py-6 top-0 z-10">
+			<nav class="flex gap-8">
+				<a href="/" class={currentPath === '/' ? 'text-custom-blue' : 'text-white'}>Home</a>
+				<a
+					href="/gettingstarted"
+					class={currentPath === '/gettingstarted' ? 'text-custom-blue' : 'text-white'}
+					>Getting Started</a
+				>
+				<a
+					href="/maps&gear"
+					class={currentPath === '/maps&gear' ? 'text-custom-blue' : 'text-white'}>Maps & Gear</a
+				>
+				<a href="/xlhub" class={currentPath === '/xlhub' ? 'text-custom-blue' : 'text-white'}
+					>XL Hub</a
+				>
+				<a
+					href="/stats&settings"
+					class={currentPath === '/stats&settings' ? 'text-custom-blue' : 'text-white'}
+					>Stats & Settings</a
+				>
+			</nav>
+		</header>
 
-	<!-- Flex Container for Sidebars and Main Content -->
-	<div class="flex justify-center w-full mx-auto">
-		<!-- Left Sidebar -->
-		<div class="hidden md:block">
-			<div class="w-64 h-full bg-blue-200 text-center p-6 mx-auto">
-				<!-- Inner Left Sidebar Content -->
-				Left Sidebar
+		<div class="flex flex-row">
+			<!-- Left Sidebar -->
+			<div class="hidden md:block">
+				<div class="w-64 h-full bg-blue-200 text-center p-6 mx-auto">
+					<!-- Inner Left Sidebar Content -->
+					Left Sidebar
+				</div>
 			</div>
-		</div>
 
-		<!-- Main Content -->
-		<div class="max-w-4xl p-6 flex flex-col items-center justify-center">
-			<slot />
-		</div>
+			<!-- Main Content -->
+			<div class="md:p-12 p-6 flex flex-col items-center">
+				<slot />
+			</div>
 
-		<!-- Right Sidebar -->
-		<div class="hidden xl:block">
-			<div class="w-64 h-full bg-blue-200 text-center p-6 mx-auto">
-				<!-- Inner Right Sidebar Content -->
-				Right Sidebar
+			<!-- Right Sidebar -->
+			<div class="hidden xl:block">
+				<div class="w-64 h-full bg-blue-200 text-center p-6 mx-auto">
+					<!-- Inner Left Sidebar Content -->
+					Right Sidebar
+				</div>
 			</div>
 		</div>
 	</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,20 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      // Define custom maxWidth
+      maxWidth: {
+        'custom-8xl': '88rem', 
+      },
+
       colors: {
         'custom-blue': '#4a9afb',
-        'custom-light-blue': '#e3e2e9', // Note: This color is the same as 'custom-light-gray'
+        'custom-light-blue': '#58b3f4', 
         'custom-black': '#101010',
         'custom-gray': '#232323',
-        'custom-light-gray': '#e3e2e9', // Duplicate of 'custom-light-blue'
+        'custom-light-gray': '#e3e2e9', 
         'custom-lime-green': '#d6f93d',
         'custom-purple': '#a77ef2',
       },
-      
     },
   },
   plugins: [],
